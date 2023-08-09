1c110eb20533e580059bf867b7b246fff2e10013
Almee98 <almeechristian6198@gmail.com>
2023-08-09 15:58:01 -0700

    Today's commit: Cleaned the code up

--- Challenges/find_numbers-master/floorplan.py
+++ Challenges/find_numbers-master/floorplan.py
@@ -34,54 +34,6 @@ class FloorPlan(ShowBase):
             camera.setP(camera.getP() - (y - base.win.getYSize() / 2) * self.mouse_sens)
         return task.cont
 
-    #     self.accept("mouse1", self.toggle_mouse_look)
-    #     self.mouse_look_enabled = False
-    #
-    #     self.camera_pitch = 0
-    #     self.camera_yaw = 0
-    #
-    #     self.setup_mouse_look()
-    #     self.setup_movement()
-    #     self.setup_rotation()
-    #
-    # def setup_mouse_look(self):
-    #     self.disable_mouse()
-    #
-    #     self.mouse_sensitivity = 0.1
-    #
-    #     self.accept("mouse-move", self.update_mouse_look)
-    #
-    # def toggle_mouse_look(self):
-    #     self.mouse_look_enabled = not self.mouse_look_enabled
-    #     if self.mouse_look_enabled:
-    #         self.enable_mouse()
-    #     else:
-    #         self.disable_mouse()
-    #
-    # def update_mouse_look(self, task):
-    #     if not self.mouse_look_enabled:
-    #         return task.cont
-    #
-    #     # Get mouse movement
-    #     dx = base.mouseWatcherNode.get_dx()
-    #     dy = base.mouseWatcherNode.get_dy()
-    #
-    #     # Apply mouse sensitivity
-    #     dx *= self.mouse_sensitivity
-    #     dy *= self.mouse_sensitivity
-    #
-    #     # Update camera pitch and yaw
-    #     self.camera_pitch -= dy
-    #     self.camera_yaw += dx
-    #
-    #     # Limit pitch to avoid camera flipping
-    #     self.camera_pitch = max(-90, min(self.camera_pitch, 90))
-    #
-    #     # Apply rotation to camera
-    #     self.camera.set_hpr(self.camera_yaw, self.camera_pitch, 0)
-    #
-    #     return task.cont
-    #
     def setup_movement(self):
         self.accept("w", self.set_key, ["forward", True])
         self.accept("a", self.set_key, ["left", True])
@@ -117,27 +69,6 @@ class FloorPlan(ShowBase):
 
         return task.cont
 
-    # def setup_rotation(self):
-    #     self.accept("x", self.rotate_model, [0, "clockwise"])
-    #     self.accept("y", self.rotate_model, [1, "clockwise"])
-    #     self.accept("z", self.rotate_model, [2, "clockwise"])
-    #     self.accept("r", self.rotate_model, [0, "counter-clockwise"])
-    #     self.accept("t", self.rotate_model, [1, "counter-clockwise"])
-    #     self.accept("v", self.rotate_model, [2, "counter-clockwise"])
-    #
-    # def rotate_model(self, axis, direction):
-    #     dt = globalClock.get_dt()
-    #     rotation_amount = self.rotation_speed * dt
-    #
-    #     if direction == "clockwise":
-    #         rotation_amount *= -1
-    #
-    #     if axis == 0:  # X-axis
-    #         self.house.set_h(self.house.get_h() + rotation_amount)
-    #     elif axis == 1:  # Y-axis
-    #         self.house.set_p(self.house.get_p() + rotation_amount)
-    #     elif axis == 2:  # Z-axis
-    #         self.house.set_r(self.house.get_r() + rotation_amount)
 
 
     def quit_program(self):
